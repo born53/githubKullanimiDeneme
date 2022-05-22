cd60f889dfef3f6eb70d2b60fa9148cd41e398b3
mustafa terzi <mszsv53@gmail.com>
2022-05-23 00:17:25 +0300

    maindeki 2.guncelleme

--- src/danemeMain.java
+++ src/danemeMain.java
@@ -1,5 +1,6 @@
 public class danemeMain {
     public static void main(String[] args) {
         System.out.println("main brancindaki ilk yazi");
+        System.out.println("maindeki 2 degisiklik");
     }
 }
