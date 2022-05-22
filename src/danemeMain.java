143f96fc0160bb23af6190bbd86c125c67559b62
mustafa terzi <mszsv53@gmail.com>
2022-05-23 00:10:32 +0300

    main branch ilk guncellenme

--- src/danemeMain.java
+++ src/danemeMain.java
@@ -1,7 +1,5 @@
-public class deneme1 {
+public class danemeMain {
     public static void main(String[] args) {
-
-        System.out.println("Deneme1 de branch olusturma calismasi");
         System.out.println("main brancindaki ilk yazi");
     }
 }
